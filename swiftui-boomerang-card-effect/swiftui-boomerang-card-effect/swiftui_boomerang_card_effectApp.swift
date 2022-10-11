932c318eb94af67af15239840e417afb1122ee4a
jcobeadev <jcobea.dev@gmail.com>
2022-10-11 14:42:34 +0800

    Setup Project

---
+++ swiftui-boomerang-card-effect/swiftui-boomerang-card-effect/swiftui_boomerang_card_effectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_boomerang_card_effectApp.swift
+//  swiftui-boomerang-card-effect
+//
+//  Created by Jco Bea on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_boomerang_card_effectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
