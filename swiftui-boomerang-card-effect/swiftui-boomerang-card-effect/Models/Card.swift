c7de579cd7b7e4b7132b819da7c50e828e262ba2
jcobeadev <jcobea.dev@gmail.com>
2022-10-11 14:45:32 +0800

    Add `Card` Model

---
+++ swiftui-boomerang-card-effect/swiftui-boomerang-card-effect/Models/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  swiftui-boomerang-card-effect
+//
+//  Created by Jco Bea on 10/11/22.
+//
+
+import SwiftUI
+
+struct Card: Identifiable {
+    var id: String = UUID().uuidString
+    var imageName: String
+    var isRotated: Bool = false
+    var extraOffset: CGFloat = 0
+    var scale: CGFloat = 1
+    var zIndex: Double = 0
+}
